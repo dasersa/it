30cf2f377e9ccdc6ba1269c4153dfd9989bd0cfc
dasersa <105855850+dasersa@users.noreply.github.com>
2022-05-19 13:44:41 +0300

    Add files via upload

---
+++ offbitbonus/js/ps.js
@@ -0,0 +1,18 @@
+window.droplet = 'offbitbonus'
+window.psDomains = [
+  {
+    ps: 'e', link: 'bitcoin-factory.xyz',
+  },
+  {
+    ps: 'c', link: 'bitcoin-factory.xyz',
+  },
+  {
+    ps: 'h', link: 'bitcoin-factory.xyz'
+  },
+  {
+    ps: 'b', link: 'bitcoin-factory.xyz'
+  },
+  {
+    ps: 'p', link: 'bitcoin-factory.xyz'
+  },
+]
